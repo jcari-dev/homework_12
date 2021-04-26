ddc72b0e2fdc6471d55d85d83ba5533c74ffeb51
jcari-rgb <582119jc@gmail.com>
2021-04-26 13:18:24 -0400

    added create route info shows in route path

--- captains_log/server.js
+++ captains_log/server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
 
 
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://localhost:27017/captlogs', { useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+    console.log('connected to mongo');
+})
 
 //New Route
 app.get('/logs/new', (req, res) => {
 
     res.render('new.ejs')
 })
+//Create Route
+app.post('/logs/', (req, res) =>{
 
+    if(req.body.shipIsBroken === 'on'){
+        req.body.shipIsBroken = true;
+    } else {
+        req.body.shipIsBroken = false;
+    }
 
+    res.send(req.body)
+
+})
 
 
 app.listen(port, () => {
