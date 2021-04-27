ea9a570e1ea06e1273dc252a066d52c1badf597e
jcari-rgb <582119jc@gmail.com>
2021-04-27 11:19:36 -0400

    added delete route

--- captains_log/server.js
+++ captains_log/server.js
@@ -5,29 +5,44 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 const Logs = require('./models/logs.js');
-app.use(express.urlencoded({extended:true}));
-
+const methodOverride = require('method-override')
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // Mongo Stuff
 
-mongoose.connect('mongodb://localhost:27017/captlogs', { useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/captlogs', { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('connected to mongo');
 })
 
 
+
 //Index Route
 
 app.get('/logs', (req, res) => {
-    
-    Logs.find({}, (error, allLogs) =>{
+
+    Logs.find({}, (error, allLogs) => {
         res.render('index.ejs', {
             logs: allLogs
-            
+
         });
     });
 });
 
+
+// Show Route
+
+
+app.get('/logs/:entry', (req, res) => {
+    //console.log(Logs.title,Logs)
+    Logs.findById(req.params.title, (err, foundLog) => {
+        //console.log(foundLog)
+        res.render('show.ejs', {
+            log: foundLog
+        })
+    })
+})
 
 
 //New Route
@@ -37,23 +52,37 @@ app.get('/logs/new', (req, res) => {
     res.render('new.ejs');
 })
 
+// Delete Route 
 
-//Create Route
+app.delete('/logs/:title', (req, res) => {
+    res.send('deleting...')
+});
 
-app.post('/logs/', (req, res) =>{
+// Create Route
 
-    if(req.body.shipIsBroken === 'on'){
+app.post('/logs/', (req, res) => {
+
+    if (req.body.shipIsBroken === 'on') {
         req.body.shipIsBroken = true;
     } else {
         req.body.shipIsBroken = false;
     }
-    Logs.create(req.body, (error, createdLogs)=>{
+    Logs.create(req.body, (error, createdLogs) => {
         res.redirect('/logs');
     })
-    
+
 
 });
 
+//Edit Route
+
+app.get('/logs/:title/edit', (req, res) => {
+    Logs.findById(req.params.title, (err, logsFound) => {
+        res.render('edit.ejs', {
+            logs: logsFound
+        });
+    });
+});
 
 //Listening Check
 
